7cd9595af553fb61d02793573aca4607f0302bc1
Guillermo Sicilia Hernandez <guillermo.sicilia@eviden.com>
2024-07-16 14:20:11 +0100

    :tokyo_tower: Eliminada las carpetas de front y back; closes #18

--- source/backend/src/main/java/com/cvorotava/backend/repository/ConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cvorotava.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cvorotava.backend.entity.Config;
-
-public interface ConfigRepository extends JpaRepository<Config, Integer> {
-
-}
